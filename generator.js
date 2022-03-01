9c3cd28074556be07923287c53b366b6d89c871d
viachess <mr.viacheslav@yandex.ru>
2022-03-02 03:01:29 +0400

    fixes initial chunk size bug and ws implementation logic, changes mock data file format from json to csv

--- generator.js
+++ generator.js
@@ -4,6 +4,7 @@ const fsPromises = fs.promises;
 const path = require("path");
 
 // const mockData = require("./mockData.json");
+// const mockCsvFile = require("./csvData.csv");
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
@@ -33,12 +34,13 @@ const getFormattedDate = (currDate) => {
   const formatted = `${year}-${month}-${date} ${hours}:${minutes}:${seconds}`;
   return formatted;
 };
-const mockDataFilePath = path.join(__dirname, "mockData.json");
+// const mockDataFilePath = path.join(__dirname, "mockData.json");
+const mockCsvFilePath = path.join(__dirname, "csvData.csv");
 
 async function generateMockData() {
-  const DATA_LIMIT = 1100000;
+  const DATA_LIMIT = 1000000;
   //   const DATA_LIMIT = 5;
-  const mockJSON = [];
+  let mockCSV = '"x","y"\n';
 
   let ms = 0;
   const divider = 5;
@@ -47,21 +49,55 @@ async function generateMockData() {
       const currDate = new Date(ms);
       // 2013-10-04 22:23:00
       const generatedDate = getFormattedDate(currDate);
-      const randomValue = getRandomInt(10, 100000);
+      const randomValue = getRandomInt(10000, 100000);
 
-      mockJSON.push({
-        x: generatedDate,
-        y: randomValue,
-      });
+      // mockJSON.push({
+      //   x: generatedDate,
+      //   y: randomValue,
+      // });
+      mockCSV += `${generatedDate},${randomValue}\n`;
       ms += 10000000;
     }
   }
   //   console.log(mockJSON);
-  await fsPromises.writeFile(mockDataFilePath, JSON.stringify(mockJSON));
+  // await fsPromises.writeFile(mockDataFilePath, JSON.stringify(mockJSON));
+  await fsPromises.writeFile(mockCsvFilePath, mockCSV);
 }
 
 generateMockData();
 // readMockData();
+const CsvParser = require("csv-parse");
+const { parse } = CsvParser;
+async function parseCsv(csvFilePath) {
+  try {
+    const records = [];
+    // Initialize the parser
+    console.log("parsing initialized");
+    const parser = parse();
+    // Use the readable stream api to consume records
+    parser.on("readable", function () {
+      let record;
+      while ((record = parser.read()) !== null) {
+        records.push(record);
+      }
+    });
+    // Catch any error
+    parser.on("error", function (err) {
+      console.error(err.message);
+    });
+    parser.on("end", function () {
+      console.log("parsing done");
+    });
+    const csvFile = await fsPromises.readFile(csvFilePath);
+    parser.write(csvFile);
+    parser.end();
+    return records;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+// parseCsv(mockCsvFilePath);
 
 async function readMockData() {
   //   for (let i = 0; i < 10; i += 1) {
@@ -69,5 +105,6 @@ async function readMockData() {
   //   }
   //   console.log(mockData[5000000]);
   // console.log(Array.isArray(mockData));
-  // await fsPromises.readFile(mockDataFilePath)
+  // await fsPromises.readFile(mockDataFilePath);
 }
+module.exports = parseCsv;
--- src/server.js
+++ src/server.js
@@ -1,15 +1,26 @@
 const express = require("express");
+const chartRoute = require("./routes/chart");
+
 const app = express();
+app.use(express.urlencoded({ extended: true, parameterLimit: 500 }));
+app.use(express.json());
+const morgan = require("./middlewares/morgan.js");
+const cors = require("./middlewares/cors.js");
+app.use(cors);
+app.use(morgan);
+
+app.use("/chart", chartRoute);
 
 const http = require("http");
 const server = http.createServer(app);
 
 const WebSocket = require("ws");
 const wss = new WebSocket.Server({ server });
+// const mockData = require("../mockData.json");
 
-const mockData = require("../mockData.json");
-
-const chartRoute = require("./routes/chart");
+const path = require("path");
+const parseCsv = require("../generator.js");
+const mockCsvFilePath = path.join(__dirname, "..", "csvData.csv");
 
 const parseMessage = (msg) => {
   return JSON.parse(msg);
@@ -20,50 +31,57 @@ const sendMessage = (ws, obj) => {
 };
 
 let currentModeName = null;
-let chunkSize = null;
+let currentChunkSize = null;
 let lastIndex = 0;
-const datasetLength = mockData.length;
 
 wss.on("connection", (ws) => {
+  // console.log("parsing result");
+  // console.log("mockData");
+  // console.log(typeof mockData);
+  // console.log(Array.isArray(mockData));
+
+  const datasetLength = mockData.length;
   console.log("connection established");
   ws.on("message", (message) => {
-    const msg = parseMessage(message);
+    const mode = parseMessage(message);
+    const { name, chunkSize, initialChunkSize } = mode;
+    console.log("new mode");
+    console.log(mode);
     // different mode => drop current state and send fresh data
-    if (msg.modeName !== currentModeName) {
-      currentModeName = msg.modeName;
-      chunkSize = msg.initialChunkSize;
-      lastIndex = msg.initialChunkSize;
+    if (name !== currentModeName) {
+      console.log(
+        `MODE CHANGE DETECTED, NEW MODE: ${name}, OLD MODE: ${currentModeName}`
+      );
+      currentModeName = name;
+      currentChunkSize = initialChunkSize;
+      lastIndex = initialChunkSize;
+      console.log(
+        `INITIAL CHUNK SIZE RECEIVED: ${initialChunkSize}, CURRENT CHUNK SIZE POST EDIT: ${currentChunkSize}`
+      );
       sendMessage(ws, {
-        data: mockData.slice(0, chunkSize + 1),
+        data: mockData.slice(1, currentChunkSize + 1),
       });
-    } else if (msg.modeName === currentModeName) {
-      if (msg.chunkSize === chunkSize) {
-        const preUpdLastIdx = lastIndex;
-        lastIndex += chunkSize;
-        if (lastIndex > datasetLength) {
-          lastIndex = chunkSize;
-          sendMessage(ws, {
-            data: mockData.slice(0, lastIndex + 1),
-          });
-        } else {
-          sendMessage(ws, {
-            data: mockData.slice(preUpdLastIdx, lastIndex + 1),
-          });
-        }
-      } else if (msg.chunkSize !== chunkSize) {
-        chunkSize = msg.chunkSize;
+    } else if (name === currentModeName) {
+      console.log(`SAME MODE DETECTED: ${name}`);
+      if (chunkSize !== currentChunkSize) {
+        console.log(
+          `CHUNK SIZE CHANGED. OLD: ${currentChunkSize} NEW: ${chunkSize}`
+        );
+        currentChunkSize = chunkSize;
+      }
+      const preUpdLastIdx = lastIndex;
+      lastIndex += chunkSize;
+      if (lastIndex > datasetLength) {
         lastIndex = chunkSize;
         sendMessage(ws, {
-          data: mockData.slice(0, lastIndex + 1),
+          data: mockData.slice(1, lastIndex),
+        });
+      } else {
+        sendMessage(ws, {
+          data: mockData.slice(preUpdLastIdx, lastIndex),
         });
       }
     }
-
-    // ws.send(
-    //   JSON.stringify({
-    //     mode: "mess",
-    //   })
-    // );
   });
   ws.on("error", (e) => {
     console.error(e);
@@ -71,22 +89,42 @@ wss.on("connection", (ws) => {
 });
 
 // --
-app.use(express.urlencoded({ extended: true, parameterLimit: 500 }));
-app.use(express.json());
-const morgan = require("./middlewares/morgan.js");
-const cors = require("./middlewares/cors.js");
-app.use(cors);
-app.use(morgan);
-
-app.use("/chart", chartRoute);
 
 // app.get("/", (req, res) => {
 //   res.status(418).json({ message: `Greetings, citizen of ${req.hostname}` });
 // });
 
 const PORT = process.env.PORT;
+const CsvParser = require("csv-parse");
+const { parse } = CsvParser;
 
-server.listen(PORT, () => {
+const fs = require("fs");
+const fsPromises = fs.promises;
+
+server.listen(PORT, async () => {
+  const records = [];
+  // Initialize the parser
+  console.log("parsing initialized");
+  const parser = parse();
+  // Use the readable stream api to consume records
+  parser.on("readable", function () {
+    let record;
+    while ((record = parser.read()) !== null) {
+      records.push(record);
+    }
+  });
+  // Catch any error
+  parser.on("error", function (err) {
+    console.error(err.message);
+  });
+  parser.on("end", function () {
+    console.log("parsing done");
+  });
+  const csvFile = await fsPromises.readFile(mockCsvFilePath);
+  parser.write(csvFile);
+  parser.end();
+  mockData = records;
+
   console.log(`Server runs on port ${PORT}`);
 });
 // server.on("upgrade", (request, socket, head) => {
