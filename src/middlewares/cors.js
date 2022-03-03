8d19edcdd8714424d74ed9a6a484d453807bed62
viachess <mr.viacheslav@yandex.ru>
2022-03-04 01:01:13 +0400

    cors test

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -5,7 +5,9 @@ let corsOptionsDelegate = function (req, callback) {
   const origins = process.env.CORS_ORIGIN;
   const originList = origins.split(",");
   let corsOptions;
-
+  console.log(`CORS LOG\n-------`);
+  console.log("ORIGIN: ", req.header("Origin"), " \n--------");
+  console.log("HOSTNAME: ", req.hostname);
   if (
     originList.indexOf(req.header("Origin")) !== -1 ||
     originList.indexOf(req.hostname) !== -1
